c5917a8d968372d7f0c2c502a809f4d1cb3a9580
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-11-05 16:15:06 +0900

    refactoring

--- src/hozumi/mongodb_session.clj
+++ src/hozumi/mongodb_session.clj
@@ -4,23 +4,23 @@
 
 (defn mongodb-store
   "Creates a mongodb session storage engine."
-  [& [collection-name]]
-  (let [collection-name (if collection-name collection-name :ring_sessions)]
-  {:read (fn [session-key]
-	   (if-let [s (and session-key
-			   (mongo/fetch-one collection-name :where {:_id session-key}))]
-	     s {}))
-   :write (fn [session-key session]
-	    (let [session (zipmap (map #(if (keyword? %) ;;work around for ::keyword -> "keyword"
-					  (-> % str (.substring 1)) %)
-				       (keys session))
-				  (vals session))]
-	      (if (and session-key (mongo/fetch-one collection-name :where {:_id session-key}))
-		(do (mongo/update! collection-name {:_id session-key} session)
-		    session-key)
-		(let [session-key (str (UUID/randomUUID))]
-		  (mongo/insert! collection-name (assoc session :_id session-key))
-		  session-key))))
-   :delete (fn [session-key]
-	     (mongo/destroy! collection-name {:_id session-key})
-	     nil)}))
+  ([] (mongodb-store :ring_sessions))
+  ([collection-name]
+     {:read (fn [session-key]
+	      (if-let [s (and session-key
+			      (mongo/fetch-one collection-name :where {:_id session-key}))]
+		s {}))
+      :write (fn [session-key session]
+	       (let [session (zipmap (map #(if (keyword? %) ;;work around for ::keyword -> "keyword"
+					     (-> % str (.substring 1)) %)
+					  (keys session))
+				     (vals session))]
+		 (if (and session-key (mongo/fetch-one collection-name :where {:_id session-key}))
+		   (do (mongo/update! collection-name {:_id session-key} session)
+		       session-key)
+		   (let [session-key (str (UUID/randomUUID))]
+		     (mongo/insert! collection-name (assoc session :_id session-key))
+		     session-key))))
+      :delete (fn [session-key]
+		(mongo/destroy! collection-name {:_id session-key})
+		nil)}))
