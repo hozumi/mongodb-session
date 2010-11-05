7b05409345694feb734098e16d292883a727a3df
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2010-11-05 11:56:51 +0900

    add lein-clojars

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [congomongo "0.1.3-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.2.1"]
+		     [lein-clojars "0.6.0"]])
